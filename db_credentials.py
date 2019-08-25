6c43c0343f4baf71694ae7947ea7050b822e89e4
Mirko Savasta <savastamirko@gmail.com>
2019-08-25 14:41:21 +0200

    Added db_credentials dictionary

---
+++ db_credentials.py
@@ -0,0 +1,10 @@
+
+## DB Credentials
+## db: default db
+config = {
+
+"SQLfwd":{"ip": "111.222.33.44", "user":"my_user","psw":None, "remote_port":22, "local_port": None},
+"ENV1":{"ip":"111.222.33.44", "user":"my_user", "psw":"password", "remote_port":3306, "local_port": 3306, "db": None },
+"ENV2":{"ip":"111.222.33.44", "user":"my_user", "psw":"password", "remote_port":3306, "local_port": 3306, "db": None }
+
+}
